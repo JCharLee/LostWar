2b838096d024b5f0d8c00fe589ee9a82175f770f
JaeSung Lee <wotjd999@gmail.com>
2022-08-12 17:38:36 +0900

    테스트용 수정

---
+++ Assets/02.Scripts/Common/DropTest.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropTest : MonoBehaviour
+{
+    private Transform playerTr;
+
+    private void Awake()
+    {
+        playerTr = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    public void ItemDrop()
+    {
+        Instantiate(Resources.Load<GameObject>("Prefabs/DropItem"), playerTr.position, Quaternion.identity);
+    }
+}
